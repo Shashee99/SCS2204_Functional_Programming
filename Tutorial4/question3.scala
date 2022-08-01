5df1a35f2c62dea1aec0c47bb2630eae2dee2791
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2022-08-01 21:24:47 +0530

    updated

--- Tutorial4/question3.scala
+++ Tutorial4/question3.scala
@@ -1,14 +1,34 @@
 object question3 {
-  val toUpper =(x:String) => x.toUpperCase()
-  val toLower = (x:String) => x.toLowerCase()
-  val firsttwo_toUpper = (x:String) => x.substring(0,2).toUpperCase()+x.substring(2,x.length)
-  val firstandlast_toUpper = (x:String) => x.substring(0,1).toUpperCase()+x.substring(1,x.length-1)+x.substring(x.length-1,x.length).toUpperCase()
+  val toUpper = (x: String) => x.toUpperCase()
+  val toLower = (x: String) => x.toLowerCase()
 
-  val formatNames=(name:String,f:(String) => String)=>f(name)
+
+  def formatNames(name: String)(indexList:Int*)(fun: (String) => String) = {
+    if (indexList.isEmpty) {
+      fun(name)
+    }
+    else {
+      var outcome: String = "";
+      var i: Int = 0;
+      while (i < name.length) {
+        if (indexList.contains(i)) {
+          outcome = outcome + fun(name.charAt(i).toString)
+        }
+        else {
+          outcome = outcome + name.charAt(i).toString
+        }
+        i = i + 1;
+      }
+      outcome
+    }
+  }
+
+
   def main(args: Array[String]): Unit = {
-    println(formatNames("Benny",toUpper))
-    println(formatNames("Niroshan",firsttwo_toUpper))
-    println(formatNames("Saman",toLower))
-    println(formatNames("Kumara",firstandlast_toUpper))
+    println(formatNames("Benny")()(toUpper))
+    println(formatNames("Niroshan")(0,1)(toUpper))
+    println(formatNames("Saman")()(toLower))
+    println(formatNames("Kumara")(5)(toUpper))
+
   }
 }
