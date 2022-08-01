c3a9a075ec630b512a385b12a790d8be2598304b
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2022-08-01 18:13:22 +0530

    updated

--- Tutorial4/question1.scala
+++ Tutorial4/question1.scala
@@ -5,7 +5,7 @@ object question1 extends App{
       case x if 0<x && x<=20000 => x*0.02
       case x if  20000<x && x<=200000 => x*0.04
       case x if 200000<x && x<=2000000 => x*0.035
-      case x if 200000<x => x*0.065
+      case x if 2000000<x => x*0.065
     }
   }
 
