695eafaec10cb39d8a390fc0b478742fb4d3e89d
Shashee99 <2020cs216@stu.ucsc.cmb.ac.lk>
2022-09-17 14:44:24 +0530

    Tutorial 8

---
+++ Tutorial8/Question1.scala
@@ -0,0 +1,34 @@
+package Tutorial8
+
+object Question1 extends App {
+
+  val p1 = Point(5, 2);
+  val p2 = Point(7, 8);
+
+  def distance(p1: Point, p2: Point): Double = {
+    val X = p1.x - p2.x;
+    val Y = p1.y - p2.y;
+
+    val d = Math.sqrt((X * X) + (Y * Y))
+    return d;
+  }
+//  add points
+  val add = p1+p2;
+  println(add)
+
+//  invert
+  val invert_add =add.invert;
+  println(invert_add);
+//  distance
+  println(distance(p1, p2))
+}
+
+
+case class Point(x: Int, y: Int) {
+  def +(that: Point) = Point(this.x + that.x, this.y + that.y);
+
+  def move(a: Int, b: Int) = Point(this.x + a, this.y + b);
+
+  def invert = Point(this.y, this.x);
+
+}
